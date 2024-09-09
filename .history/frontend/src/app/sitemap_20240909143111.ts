c0a020c1ae929c04abfda6f9309c3ec6f27b7c63
Thomas Musial <th896493@dal.ca>
2024-09-09 14:33:07 -0300

    adding site map and robots

---
+++ .history/frontend/src/app/sitemap_20240909143111.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://www.ontariocashforhouses.com/',
+      lastModified: new Date('2024-08-10'),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url:  'https://www.ontariocashforhouses.com/process',
+      lastModified: new Date('2024-08-10'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.focusflowsoftware.com/contact',
+      lastModified: new Date('2024-08-10'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.ontariocashforhouses.com/'/about',
+      lastModified: new Date('2024-08-10'),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
